347adc7ca20a8a2db5edf2d0e0fb808686f22211
Barun Ghosh <barungh@hotmail.com>
2021-09-09 06:49:21 +0530

    :zap: Structs in rust
    structs in rust seems like interface of typescript

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,21 @@
 fn main() {
-    for n in -2..5 {
-        println!("{} is {}.", n, match n {
-            0 => "zero",
-            1 => "one",
-            _ if n < 0 => "negative", // All other cases 'if' lower than 0
-            _ => "positive", // and all other cases, here all positive numbers
-        });
-    }
+    
+struct SomeData {
+    i: i32,
+    f: f32,
+    c: char,
+    five_bytes: [u8; 5],
 }
 
-// fn main() {
-//     let outcome = Result::Success(43.78);
-//     print_result(outcome);
-//     let outcome = Result::Failure(102, 'Y');
-//     print_result(outcome);
-//     let outcome = Result::Uncertainty;
-//     print_result(outcome); // This line will print nothing
-// }
-// 
-// enum Result {
-//    Success(f64),
-//    Failure(u16, char),
-//    Uncertainty,
-// }
-// 
-// fn print_result(outcome: Result) {
-//     match outcome {
-//        Result::Success(_) => println!("OK"), // _ underscore sign , lets skip variable
-//        Result::Failure(error_code, module) =>
-//            println!("Error no. {} in module {}", 
-//                     error_code, module),
-//        Result::Uncertainty => {},
-//     }
-// }
+let data = SomeData {
+    i: 10_000_000,
+    f: 134.23,
+    c: 'T',
+    five_bytes: [9, 0, 3, 43, 130],
+};
+print!("{} {} {} {}", data.five_bytes[3], data.i,
+       data.f, data.c);
+}
+
+
+
